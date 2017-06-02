0e0367aac306714fe1608c7dd79568bc4c6be505
Luiz Carlos <luizgadao@gmail.com>
2017-06-02 17:49:53 -0300

    add test for find similar notes

--- app/src/test/java/com/jwhh/jim/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/jwhh/jim/notekeeper/DataManagerTest.java
@@ -9,13 +9,17 @@ import static org.junit.Assert.assertEquals;
  * Created by Luiz on 02/06/17.
  */
 public class DataManagerTest {
+    private DataManager mDataManager;
+
     @Before
     public void setUp() throws Exception {
+        mDataManager = DataManager.getInstance();
+        mDataManager.getNotes().clear();
+        mDataManager.initializeExampleNotes();
     }
 
     @Test
     public void createNewNote() throws Exception {
-        DataManager mDataManager = DataManager.getInstance();
         CourseInfo mCourse = mDataManager.getCourse("android_async");
         String mNoteTitle = "Test note title";
         String mNoteText = "This is the body text of my test note";
@@ -30,6 +34,29 @@ public class DataManagerTest {
         assertEquals(mCompareNote.getCourse(), mCourse);
         assertEquals(mCompareNote.getTitle(), mNoteTitle);
         assertEquals(mCompareNote.getText(), mNoteText);
+    }
+
+    @Test
+    public void findSimilarNotes() throws Exception {
+        CourseInfo mCourse = mDataManager.getCourse("android_async");
+        String mNoteTitle = "Test note title";
+        String mNoteText = "This is the body text of my test note";
+        String mNoteText2 = "This is the body text of my test note 2";
+
+        int mNoteIndex = mDataManager.createNewNote();
+        NoteInfo mNewNote = mDataManager.getNotes().get(mNoteIndex);
+        mNewNote.setCourse(mCourse);
+        mNewNote.setTitle(mNoteTitle);
+        mNewNote.setText(mNoteText);
+
+        int mNoteIndex2 = mDataManager.createNewNote();
+        NoteInfo mSecondNote = mDataManager.getNotes().get(mNoteIndex2);
+        mSecondNote.setCourse(mCourse);
+        mSecondNote.setTitle(mNoteTitle);
+        mSecondNote.setText(mNoteText2);
+
+        assertEquals(mNoteIndex, mDataManager.findNote(mNewNote));
+        assertEquals(mNoteIndex2, mDataManager.findNote(mSecondNote));
     }
 
 }

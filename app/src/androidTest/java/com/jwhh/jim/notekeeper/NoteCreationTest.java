221179bd9f8d0ad20b2561e07b557dc8a07fef20
Luiz Carlos <luizgadao@gmail.com>
2017-06-05 15:04:57 -0300

    test add mathcer in spinner

--- app/src/androidTest/java/com/jwhh/jim/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/jwhh/jim/notekeeper/NoteCreationTest.java
@@ -3,15 +3,22 @@ package com.jwhh.jim.notekeeper;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.hamcrest.core.AllOf;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 
 /**
  * Created by Luiz on 02/06/17.
@@ -19,14 +26,33 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
 
+    private static DataManager sDatamanager;
+
+    @BeforeClass
+    public static void setUpClass(){
+        sDatamanager = DataManager.getInstance();
+    }
+
     @Rule
     public ActivityTestRule<NoteListActivity> mActivityActivityTestRule =
             new ActivityTestRule<>(NoteListActivity.class);
 
     @Test
     public void createNewNote(){
+
+        CourseInfo mCourseInfo = sDatamanager.getCourse("java_lang");
+
+        String title = "note title";
+        String subTitle = "note text description";
+
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_note_title)).perform(typeText("note title"));
-        onView(withId(R.id.text_note_text)).perform(typeText("note text description"), closeSoftKeyboard());
+
+        onView(withId(R.id.spinner_courses)).perform(click());
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(mCourseInfo))).perform(click());
+
+        onView(withId(R.id.text_note_title)).perform(typeText(title));
+        onView(withId(R.id.text_note_text)).perform(typeText(subTitle), closeSoftKeyboard());
+
+        pressBack();
     }
 }
